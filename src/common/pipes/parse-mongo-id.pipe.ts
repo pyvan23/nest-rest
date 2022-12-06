40638aa45fcc12c8de093baf928c0b1fb2ff7703
pyvan23 <ivanmiguelalvarez@gmail.com>
2022-12-06 12:15:45 -0300

    esLint

--- src/common/pipes/parse-mongo-id.pipe.ts
+++ src/common/pipes/parse-mongo-id.pipe.ts
@@ -1,19 +1,21 @@
-import { ArgumentMetadata, Injectable, PipeTransform, BadRequestException } from '@nestjs/common';
-import { isValidObjectId } from 'mongoose';
+/* eslint-disable prettier/prettier */
+import {
+  ArgumentMetadata,
+  Injectable,
+  PipeTransform,
+  BadRequestException,
+} from "@nestjs/common";
+import { isValidObjectId } from "mongoose";
 
 @Injectable()
 export class ParseMongoIdPipe implements PipeTransform {
-  
-  
   transform(value: string, metadata: ArgumentMetadata) {
     // console.log({ value, metadata });
-    
-    if ( !isValidObjectId(value) ) {
-      throw new BadRequestException(`${ value } is not a valid MongoID`);
+
+    if (!isValidObjectId(value)) {
+      throw new BadRequestException(`${value} is not a valid MongoID`);
     }
 
     return value;
   }
-
-
 }
